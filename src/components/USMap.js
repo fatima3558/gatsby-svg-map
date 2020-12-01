59d1c1bf9115f5e5179173a8830cbca6d7741c99
Fatima Gomez <fatima.gomez@datamade.us>
2020-12-01 11:26:09 -0600

    map renders

---
+++ src/components/USMap.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { SVGMap } from "react-svg-map"
+import SouthKorea from "@svg-maps/south-korea"
+
+const USMap = () => {
+  return <SVGMap map={SouthKorea} />
+}
+
+export default USMap
