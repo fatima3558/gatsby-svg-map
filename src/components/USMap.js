448346669ce3cd10e629b2097fee697f03b88543
Fatima Gomez <fatima.gomez@datamade.us>
2020-12-02 14:39:27 -0600

    can get div displaying mouseover region name above map

--- src/components/USMap.js
+++ src/components/USMap.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { SVGMap } from "react-svg-map"
 import SouthKorea from "@svg-maps/south-korea"
 
@@ -12,18 +12,52 @@ const newMap = () => {
   return regions
 }
 
-const mouseOver = (event) => {
+const mouseOver = (event, initialState, setState) => {
   const current = event.target
   current.setAttribute('style', 'fill:red')
 }
 
-const mouseOut = (event) => {
+const mouseOut = (event, initialState, setState) => {
   const current = event.target
   current.setAttribute('style', '')
+
+  const activeRegion = event.target.getAttribute('name')
+  const tooltipStyle = {display: 'none'}
+
+  setState({ activeRegion, tooltipStyle})
+}
+
+const mouseMove = (event, initialState, setState) => {
+  const activeRegion = event.target.getAttribute('name')
+  const tooltipStyle = {
+    display: 'block',
+    top: event.clientY + 10,
+    left: event.clientX - 100
+  };
+
+  setState({ activeRegion });
+
 }
 
 const USMap = () => {
-  return <SVGMap map={newMap()} onLocationMouseOver={mouseOver} onLocationMouseOut={mouseOut}/>
+  const [state, setState] = useState({
+    activeRegion: null,
+    tooltipStyle: {
+      display: 'none'
+    }})
+  return (
+    <div>
+      <div style={state.tooltipStyle}>
+        {state.activeRegion}
+      </div>
+      <SVGMap 
+        map={newMap()}
+        onLocationMouseOver={(e) => mouseOver(e, state, setState)}
+        onLocationMouseOut={(e) => mouseOut(e, state, setState)}
+        onLocationMouseMove={(e) => mouseMove(e, state, setState)}
+      />
+    </div>
+    )
 }
 
 export default USMap
