b51ce62a186d7ccc33effa5381d915045975e787
Fatima Gomez <fatima.gomez@datamade.us>
2020-12-02 10:45:29 -0600

    can interactively change region styling

--- src/components/USMap.js
+++ src/components/USMap.js
@@ -13,28 +13,17 @@ const newMap = () => {
 }
 
 const mouseOver = (event) => {
-  console.log(event.target.getAttribute('name'))
+  const current = event.target
+  current.setAttribute('style', 'fill:red')
 }
 
-function componentDidMount() {
-  for(let i = 0; i < SouthKorea.locations; i++) {
-    const currentLocation = SouthKorea.locations[i]
-    if(i % 5 === 0) {
-      console.log("violet")
-    } else if(i % 4 === 0) {
-      console.log("blue")
-    } else if(i % 3 === 0) {
-      console.log("green")
-    } else if(i % 2 === 0) {
-      console.log("yellow")
-    } else {
-      console.log("red")
-    }
-  }
+const mouseOut = (event) => {
+  const current = event.target
+  current.setAttribute('style', '')
 }
 
 const USMap = () => {
-  return <SVGMap map={newMap()} onLocationMouseOver={mouseOver}/>
+  return <SVGMap map={newMap()} onLocationMouseOver={mouseOver} onLocationMouseOut={mouseOut}/>
 }
 
 export default USMap
